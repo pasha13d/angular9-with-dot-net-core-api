6af6b941e2a9f904ffe8b04f24eb0c4c49f013ac
pasha13d <mahabub13d1995@gmail.com>
2020-10-04 09:35:43 +0600

    SCP parent

---
+++ SCP/SCP/Context/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SCP.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SCP.Context
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Company { get; set; }
+    }
+}
---
+++ SCP/SCP/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SCP.Context;
+using SCP.Model;
+
+namespace SCP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public CompanyController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [Route("GetAllCompany")]
+        [HttpGet]
+        public ActionResult<List<Company>> GetAllCompany()
+        {
+            var companyList = _context.Company.ToList();
+            return Ok(companyList);
+        }
+    }
+}
